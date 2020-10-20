237ed066fe66c79a9b354b855f6690d0874ac111
文帅 <1356561149@qq.com>
2020-10-20 17:55:45 +0800

    函数名修改

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const download = require('download')
 
-getWeb()
+getImgs()
 
-async function getWeb() {
+async function getImgs() {
   const res = await axios.get('https://www.xiniaogongkao.com/teacher')
   const $ = cheerio.load(res.data)
   $('.card .card-top img').each(async (index, el) => {
